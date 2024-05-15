8a4d67389a0cee0a36d026045fea5710976e9a66
AgenteBP <104331234+AgenteBP@users.noreply.github.com>
2024-05-15 19:26:54 -0300

    Subida Proyecto

---
+++ mercadolibre_back/src/main/java/com/kingsman/mercadolibre_back/MercadolibreBackApplication.java
@@ -0,0 +1,13 @@
+package com.kingsman.mercadolibre_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadolibreBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadolibreBackApplication.class, args);
+	}
+
+}
---
+++ mercadolibre_back/src/test/java/com/kingsman/mercadolibre_back/MercadolibreBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingsman.mercadolibre_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadolibreBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
