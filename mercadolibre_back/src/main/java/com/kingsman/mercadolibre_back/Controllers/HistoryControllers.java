55e53abf2b595ef5c2ddabf4030f25d2680aaadf
AgenteBP <104331234+AgenteBP@users.noreply.github.com>
2024-05-29 18:07:09 -0300

    Update 29/5/2024
    Se agregaron funciones para el User, Article, History

--- mercadolibre_back/src/main/java/com/kingsman/mercadolibre_back/Controllers/HistoryControllers.java
+++ mercadolibre_back/src/main/java/com/kingsman/mercadolibre_back/Controllers/HistoryControllers.java
@@ -1,10 +1,49 @@
 package com.kingsman.mercadolibre_back.Controllers;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.kingsman.mercadolibre_back.Services.HistoryServices;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/history")
 public class HistoryControllers {
+    private final int DEFAULT_PAGE_NUMBER = 0;
+    private final int DEFAULT_QUANTITY_PER_PAGE = 10;
+
+    @Autowired
+    private HistoryServices historyServices;
+
+    @GetMapping("/see")
+    public ResponseEntity<?> getMovementsController(@RequestParam Map<String, String> map) {
+        int page = map.containsKey("page") ? Integer.parseInt(map.get("page")) : DEFAULT_PAGE_NUMBER;
+        int quantityPerPage = map.containsKey("quantity") ? Integer.parseInt(map.get("quantity")) : DEFAULT_QUANTITY_PER_PAGE;
+        String userEmail = map.get("email");
+        Page<Object[]> history;
+
+        if(!userEmail.isEmpty()){
+            history = historyServices.getMovements(page, quantityPerPage, userEmail);
+
+            if(!history.getContent().isEmpty()){
+                return new ResponseEntity<>(history, HttpStatus.OK);
+            }else{
+                return new ResponseEntity<String>("Empty History", HttpStatus.NO_CONTENT);
+            }
+            
+        }
+        else{
+            return new ResponseEntity<>("Email parameter is missing", HttpStatus.BAD_REQUEST);
+        }
+
+    }
+    
     
 }
--- mercadolibre_back/src/main/java/com/kingsman/mercadolibre_back/Repositories/HistoryRepositories.java
+++ mercadolibre_back/src/main/java/com/kingsman/mercadolibre_back/Repositories/HistoryRepositories.java
@@ -1,8 +1,18 @@
 package com.kingsman.mercadolibre_back.Repositories;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.kingsman.mercadolibre_back.Models.History;
 
 public interface HistoryRepositories extends JpaRepository<History, Integer>{
+
+    @Query(nativeQuery = true, value = "SELECT h.* FROM history h " +
+                                       "JOIN user u ON h.id_selling_user = u.id " +
+                                       "WHERE u.email = :email")
+    Page<Object[]> getMovementsHistory(Pageable pageable, @Param("email") String userEmail);
     
 }
